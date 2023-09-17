f37b985f7d48d272acdc5ea675c0416a71e9e4be
jorgepvasconcelos <jorge.r.p.v@hotmail.com>
2023-09-17 11:59:49 -0300

    improve logging

--- crawler_manager/logger.py
+++ crawler_manager/logger.py
@@ -1,19 +1,9 @@
-import argparse
 import logging
-# import logging.config
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-log',
-                    '--loglevel',
-                    default='info',
-                    help='Provide logging level. Example --loglevel debug, default=info')
-args = parser.parse_args()
 logger = logging.getLogger('crawler_manager')
-logger.setLevel(args.loglevel.upper())
+logger.setLevel(logging.INFO)
 
 ch = logging.StreamHandler()
-ch.setLevel(args.loglevel.upper())
-
 formatter = logging.Formatter('[%(asctime)s] [%(levelname)s] %(message)s', datefmt='%m-%d-%Y %H:%M:%S')
 ch.setFormatter(formatter)
 logger.addHandler(ch)
