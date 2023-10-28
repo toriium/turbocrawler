40c8944c308465c7fe05c4e864650eb7b4cbfb20
jorgepvasconcelos <jorge.r.p.v@hotmail.com>
2023-10-28 17:11:47 -0300

    feat: add LOG class

--- turbocrawler/logger.py
+++ turbocrawler/logger.py
@@ -1,15 +1,25 @@
 import logging
 import os
 
+from turbocrawler.utils import create_file_path
+
 formatter = logging.Formatter('%(asctime)s|%(levelname)s| %(message)s', datefmt='%m-%d-%Y %H:%M:%S')
 
-logger = logging.getLogger('turbocrawler')
+
+class LOG(logging.Logger):
+    def create_console_handler(self):
+        console_handler = logging.StreamHandler()
+        console_handler.setFormatter(formatter)
+        self.addHandler(console_handler)
+
+    def create_file_handler(self, filename):
+        path = f'{os.getcwd()}/crawler_logs/{filename}.txt'
+        create_file_path(path)
+        file_handler = logging.FileHandler(path, mode='w')
+        file_handler.setFormatter(formatter)
+        self.addHandler(file_handler)
+
+
+logger = LOG('turbocrawler')
 logger.setLevel(logging.INFO)
-
-console_handler = logging.StreamHandler()
-console_handler.setFormatter(formatter)
-logger.addHandler(console_handler)
-
-file_handler = logging.FileHandler(f'{os.getcwd()}/log.txt')
-file_handler.setFormatter(formatter)
-logger.addHandler(file_handler)
+logger.create_console_handler()
